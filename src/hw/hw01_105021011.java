1dc88ca4b07e976daa89de5effc457035aedffeb
Leisure123 <105021011@live.asia.edu.tw>
2016-09-27 20:24:12 +0800

    Week02作業繳交

--- src/hw/hw01_105021011.java
+++ src/hw/hw01_105021011.java
@@ -3,7 +3,8 @@ package hw;
  * Topic: 假設銅板有1元、5元、50元共三種，媽媽請小明去菜市場買水果，給了小明N元，且媽媽交待，要老闆找小明的零錢的數目要最少，
  * 而小明到了水果攤買了a1顆蘋果，a2顆柳丁，及a3顆桃子，1顆蘋果15元，1顆柳丁20元，1顆桃子30元，請問老問需找多少個1元、5元、
  * 50元，其銅板數目最少。
- * (先輸入媽媽給小明多少錢，N，接著輸入a1, a2, a3 ， 在此n, a1, a2, a3為整數，且a1*15+ a2*20+ a3*30小於或等於N。列出共找小明多少個1元，5元及50元，若帶的錢不夠買水果，則顯示”0”)
+ * (先輸入媽媽給小明多少錢，N，接著輸入a1, a2, a3 ， 在此n, a1, a2, a3為整數，且a1*15+ a2*20+ a3*30小於或等於N。
+ * 列出共找小明多少個1元，5元及50元，若帶的錢不夠買水果，則顯示”0”)
  * Date: 2016/09/26
  * Author: 105021011 丁紹剛
  */
@@ -13,6 +14,25 @@ public class hw01_105021011 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
+		System.out.print("媽媽給了:");
+		int n = scn.nextInt();
+		System.out.print("蘋果買了:");
+		int a1 = scn.nextInt();
+		System.out.print("柳丁買了:");
+		int a2 = scn.nextInt();
+		System.out.print("桃子買了:");
+		int a3 = scn.nextInt();
+		int x = a1*15 + a2*20 + a3*30;
+		int fifty1 = x / 50;
+		int fifty2 = x % 50;
+		int five1 = fifty2 / 5;
+		int five2 = fifty2 % 5;
+		if(x >= n){
+			System.out.println("0");
+		}else{
+			System.out.print("50元找了:" + fifty1 + "\n 5元找了:" + five1 + "\n 1元找了:" + five2);
+		}
+		
 
 	}
 
--- src/hw/hw02_105021011.java
+++ src/hw/hw02_105021011.java
@@ -1,6 +1,7 @@
 package hw;
 /*
- * Topic:已知男生標準體重＝(身高－80 )*0.7；女生標準體重＝(身高－70)*0.6；試寫一個程式可以計算男生女生的標準體重。(輸入兩個數值，依序代表為身高及性別（1代表男性；2代表女性）)
+ * Topic:已知男生標準體重＝(身高－80 )*0.7；女生標準體重＝(身高－70)*0.6；
+試寫一個程式可以計算男生女生的標準體重。(輸入兩個數值，依序代表為身高及性別（1代表男性；2代表女性）)
  * Date: 2016/09/26
  * Author: 105021011 丁紹剛
  */
@@ -14,14 +15,19 @@ public class hw02_105021011 {
 		int gender = scn.nextInt();
 		System.out.print("請輸入身高:");
 		int w = scn.nextInt();
-		if(gender == 1){
+		if(gender != 1&& gender != 2){
+			System.out.println("請不要輸入其他性別!");
+		}else{
+			if(gender == 1){
 			float sw = (w - 80)*0.7f;
 			System.out.print("你的標準體重為:" + sw);
 		}else{
 			float sw = (w -70)*0.6f;
 			System.out.print("你的邊準體重為:" + sw);
-		}
+			
+			}
+			
 
 	}
-
+	}
 }
